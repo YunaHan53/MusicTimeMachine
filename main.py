fce2d4cb2b0a714a276125f02e16afcd7512fc9d
YunaH <Yunahan53@gmail.com>
2026-04-09 15:29:42 -0400

    Added new search class with get_artist_id and get_song_uri functions. Created a song_uri_list in main.py.

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import billboard
-import os
-from dotenv import load_dotenv
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from search import SpotifySearch
 
-load_dotenv()
+search = SpotifySearch()
 
-date = input("Which year do you want to travel to? Enter the date in the format YYYY-MM-DD: ")
+# Billboard.py top 100 songs chart by date
+date = input("Which year do you want to travel to? Enter the date in this format: YYYY-MM-DD :")
 chart = billboard.ChartData("hot-100", date)
-print(chart)
+song_uri_list = []
 
-client_id = os.getenv("SPOTIFY_CLIENT_ID")
-client_secret = os.getenv("SPOTIFY_SECRET")
-redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
+for song in chart:
+    artist_id = SpotifySearch.get_artist_id(search, song.artist)
+    if artist_id is None:
+        continue
+    else:
+        artist_uri = f"spotify:artist:{artist_id}"
+        song_uri = SpotifySearch.get_song_uri(search, song.artist, song.title)
+        song_uri_list.append(song_uri)
+        print(f"Artist: {song.artist}, Artist URI: {artist_uri}, Song: {song.title}, Song URI: {song_uri}")
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id=client_id,
-    client_secret=client_secret,
-    redirect_uri=redirect_uri,
-    scope="user-library-read"))
-
-results = sp.current_user_saved_tracks(limit=5)
-print(results)
+print(song_uri_list)
---
+++ search.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+
+class SpotifySearch:
+    def __init__(self):
+        load_dotenv()
+        self.client_id = os.getenv('SPOTIFY_CLIENT_ID')
+        self.client_secret = os.getenv('SPOTIFY_SECRET')
+        self.redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI')
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= self.client_id,
+                                                            client_secret= self.client_secret,
+                                                            redirect_uri= self.redirect_uri,
+                                                            scope="user-library-read"))
+        self.user_id = self.sp.current_user().get("id")
+
+    def get_artist_id(self,artist_name):
+        artist_search = self.sp.search(q=f"artist:{artist_name}", type="artist", limit=1)
+        items = artist_search["artists"]["items"]
+        if not items:
+            print(f"Artist {artist_name} not found")
+            return None
+
+        artist_id = items[0]["id"]
+        return artist_id
+
+    def get_song_uri(self,artist_name, song_name):
+        song_search = self.sp.search(q=f"track:{song_name}", type="track")
+        items = song_search["tracks"]["items"]
+        if not items:
+            print(f"Song {song_name} not found")
+            return None
+
+        # artist = items[0]["album"]["artists"][0]["name"]
+        # song = items[0]["name"]
+        # print(f"Artist: {artist}, Song: {song}")
+
+        song_uri = items[0]["uri"]
+        return song_uri
